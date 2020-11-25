eb0e7004163ba049c9c0780d0a5ee6e1b9a55033
Matheus Mazoni <mmazoni.andrade@gmail.com>
2020-11-25 08:25:05 -0300

    listar usuário na resenha(home)

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -6,11 +6,15 @@
 module Handler.Home where
 
 import Import
-
+import Database.Persist.Sql
 
 getHomeR :: Handler Html
 getHomeR = do
-    resenhas <- runDB $ selectList [] [Asc ResenhaId]
+    --resenhas <- runDB $ selectList [] [Asc ResenhaId]
+    let sql = "select ??, ?? \
+            \ from resenha inner join usuario \
+            \ on resenha.usuario_id = usuario.id"
+    resenhas <- runDB $ rawSql sql []
     defaultLayout $ do
         setTitle "MyLibrary"
         $(widgetFile "homepage")
