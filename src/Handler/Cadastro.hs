7bc86accffbab07bd7d1ce92a7e51adc2b809409
Matheus Mazoni <mmazoni.andrade@gmail.com>
2020-11-25 07:02:26 -0300

    validação da existência do email no cadastro

--- src/Handler/Cadastro.hs
+++ src/Handler/Cadastro.hs
@@ -30,17 +30,26 @@ postCadastroR = do
     ((result,_),_) <- runFormPost formUsu
     case result of
         FormSuccess (usuario,verifica) -> do
-            if (usuarioSenha usuario == verifica) then do
-                _ <- runDB $ insert usuario
-                setMessage [shamlet|
-                    <div .alert.alert-success role="alert">
-                        Cadastro com sucesso.
-                |]
-                redirect CadastroR
-            else do
-                setMessage [shamlet|
-                    <div .alert.alert-danger role="alert">
-                        Sem sucesso no cadastro.
-                |]
-                redirect CadastroR
+            user <- runDB $ getBy (UniqueEmail (usuarioEmail usuario))
+            case user of
+                Nothing -> do
+                    if (usuarioSenha usuario == verifica) then do
+                        _ <- runDB $ insert usuario
+                        setMessage [shamlet|
+                            <div .alert.alert-success role="alert">
+                                Cadastro com sucesso.
+                        |]
+                        redirect EntrarR
+                    else do
+                        setMessage [shamlet|
+                            <div .alert.alert-danger role="alert">
+                                Sem sucesso no cadastro.
+                        |]
+                        redirect CadastroR
+                Just(Entity _ _) -> do
+                    setMessage [shamlet|
+                        <div .alert.alert-danger role="alert">
+                            Email já foi utilizado.
+                    |]
+                    redirect CadastroR
         _ -> redirect HomeR
